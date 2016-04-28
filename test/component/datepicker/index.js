b88f7cb7134d4d56f88a8b5c63495248bb83c7fa
Jason Wang <JasonWang0421@gmail.com>
2016-04-28 10:54:59 +0800

    remove unused console info

--- test/component/datepicker/index.js
+++ test/component/datepicker/index.js
@@ -9,7 +9,7 @@ const DatePicker = require("./../../../src/component/datepicker/index");
 
 $(function () {
 
-    const instance = Scope.render(
+    Scope.render(
         <DatePicker date={{ year: 2016, month: 4, day: 21 }}
                     dayRule={function(year, month, day){
                         if(day == 13){ return false; }
@@ -20,6 +20,5 @@ $(function () {
         />,
         document.getElementById("container")
     );
-    console.log(instance);
 
 });
